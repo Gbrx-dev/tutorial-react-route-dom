8eb2d690a2120f9a877048fff5e1f7f1aa6f5417
Gbrx Dev <gabrielhenriquex1566@gmail.com>
2026-03-11 11:01:41 -0300

    Initial commit

---
+++ src/main.tsx
@@ -0,0 +1,21 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './styles/global.css'
+
+
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import { About } from './components/About';
+import { Home } from './components/Home'
+import { Menu } from './components/Menu';
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <BrowserRouter>
+    <Menu/>
+    <Routes>
+      <Route path='/' element={<Home />}/>
+      <Route path='/about' element={<About />}/>
+    </Routes>
+    </BrowserRouter>
+  </StrictMode>,
+)
